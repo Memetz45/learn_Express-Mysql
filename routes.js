de7448d1dfe960f3b464604ab71a0286fc128320
Memetz45 <slametnurhidayat45@gmail.com>
2023-03-05 22:15:57 +0700

    menambahkan req.file dan req.body untuk menerima request input

--- routes.js
+++ routes.js
@@ -6,30 +6,40 @@ const path = require('path');
 
 router.get('/', (req, res) => {
     res.send(
-        `<h1 style= "text-align: center;">Tugas Express JS Eduwork</h1>
-        <form>
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname">
-      </form>`
+        `<div style= "text-align: center;">
+        <h1>Tugas Express JS Eduwork</h1>
+        <form action="/product" enctype="multipart/form-data" method="post">
+        <label for="fname">Name:</label><br>
+        <input type="text" name="name" placeholder="Masukan nama produk"><br>
+        <label for="lname">Price:</label><br>
+        <input type="text"  name="price" placeholder="Masukan harga produk"><br>
+        <label for="fname">Stock:</label><br>
+        <input type="text"  name="stock" placeholder="Masukan stock produk"><br>
+        <label for="lname">Image:</label><br>
+        <input type="file"  name="image"><br><br>
+        <button type="submit">Submit</button>
+        </form>
+        </div>`
     )
 });
 
-router.post('/product/', upload.single('image'), (req, res) => {
-    const {name, price, stock, status} = req.body;
+router.post('/product', upload.single('image'), (req, res) => {
+    const {name, price, stock} = req.body;
+    console.log(req.body);
     const image = req.file;
+    console.log(req.file);
     if(image) {
         const target = path.join(__dirname, 'uploads', image.originalname);
         fs.renameSync(image.path, target)
-        res.json({
-            name,
-            price,
-            stock,
-            status,
-            image
-        });
+        res.send("recieved your request!");
+        // res.json({
+        //     name,
+        //     price,
+        //     stock,
+        //     image
+        // });
     }
+
 });
 
 module.exports = router;
