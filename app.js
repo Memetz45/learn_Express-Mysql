103abfbaff9a3a6dc77cc7283784e1b67f924ec4
Memetz45 <slametnurhidayat45@gmail.com>
2023-03-24 02:10:23 +0700

    memperbaiki

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require ('express');
+const path = require ('path');
 const app = express();
 const router = require('./routes');
+const productRouter = require ('./app/product/routes');
+const productRouterV2 = require ('./app/product_v2/router');
+const logger = require ('morgan')
 
+
+app.use(logger('dev'));
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'iploads')));
+app.use('/api/v1', productRouter);
 app.use(router);
+app.use((req, res) => {
+    res.status(404);
+    res.send({
+        status: 'failed',
+        message: 'Resource ' + req.originalUrl + ' Not Found'
+    })
+})
 
 app.listen(3001, () => console.log('Server: http://localhost:3001'));
--- app/product/routes.js
+++ app/product/routes.js
@@ -3,7 +3,8 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads' });
 const productController = require('./controler');
 
-// router.get('/product', productController.index);
+
+router.get('/product', productController.index);
 
 router.get('/product/:id', productController.view);
 
